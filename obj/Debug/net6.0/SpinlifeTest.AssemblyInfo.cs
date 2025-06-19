aacec70902836a350182448b866914091ab8fcf8
Aniket Bhagat <aniket.bhagat@spinlife.com>
2025-06-19 17:48:52 +0530

    checkout page update

--- obj/Debug/net6.0/SpinlifeTest.AssemblyInfo.cs
+++ obj/Debug/net6.0/SpinlifeTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpinlifeTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ef908aafb989428ec8edbb31af6db1010418d78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f8deaef10305775d8f86b1958c57f89ba737ac7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpinlifeTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpinlifeTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
